a461d52f83a4f83214015012693d7816ecfb08f5
jumawilliam2011@gmail <jumawilliam2011@gmail.com>
2020-03-21 22:00:55 +0300

    updating product created

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+	protected $fillable=[
+		'name','price','detail','discount'];
     public function reviews(){
     	return $this->hasMany('App\Review');
     }
