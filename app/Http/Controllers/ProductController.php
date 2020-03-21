13c536b3362f12175f2a4d4a5dfac5f595bcf786
jumawilliam2011@gmail <jumawilliam2011@gmail.com>
2020-03-21 21:26:54 +0300

    Created product request, storing a product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,13 @@ use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductCollection;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,11 +39,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
-    }
-
+        $product=new Product;
+        $product->name=$request->name;
+        $product->detail=$request->detail;
+        $product->discount=$request->discount;
+        $product->price=$request->price;
+        $product->stock=$request->stock;
+        $product->save();
+        return response([
+            'data'=> new ProductResource($product)
+        ],201);
+     }
     /**
      * Display the specified resource.
      *
